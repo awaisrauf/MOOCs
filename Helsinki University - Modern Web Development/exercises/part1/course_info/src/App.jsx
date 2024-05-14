c9699d100c85c9fac421e904ffc68d6801dd1286
awais <30290500+awaisrauf@users.noreply.github.com>
2024-05-15 00:48:18 +0400

    hw part 1.5 of Helsinki Full Stack App Developement

--- Helsinki University - Modern Web Development/exercises/part1/course_info/src/App.jsx
+++ Helsinki University - Modern Web Development/exercises/part1/course_info/src/App.jsx
@@ -34,26 +34,28 @@ const Total = ({parts}) => {
 }
 
 const App = () => {
-    const course = 'Half Stack application developement'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
+    const course = {
+        name: 'Half Stack application developement',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
     return (
         <div>
-            <Header course={course} />
-            <Content parts={parts}/>
-            <Total parts={parts}/>
+            <Header course={course.name} />
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
         </div>
     )
 

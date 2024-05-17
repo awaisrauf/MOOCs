b99ef313889a0dac0bdccfc82d5f6c0f4f9f6e0d
awais <30290500+awaisrauf@users.noreply.github.com>
2024-05-18 00:25:07 +0400

    part 1, unicafe, 1.11 completed

--- Helsinki University - Modern Web Development/exercises/part1/unicafe/README.md
+++ Helsinki University - Modern Web Development/exercises/part1/unicafe/README.md
@@ -2,6 +2,7 @@
 
 A small website to show student feedack for university cafe.
 
+![](public/final.png)
 ## Run
 
 ```sh
--- Helsinki University - Modern Web Development/exercises/part1/unicafe/src/App.jsx
+++ Helsinki University - Modern Web Development/exercises/part1/unicafe/src/App.jsx
@@ -1,31 +1,5 @@
 import {useState} from 'react'
 
-// const ShowAvg = ({good, neutral, bad}) => {
-//   const total = good + neutral + bad
-//   const  avg = ((1*good) + (-1*bad))/total
-//   return(
-//     <> 
-//     <p>avg {avg}</p>
-//     </>
-//   )
-// }
-
-// const ShowPosPercent = ({good, neutral, bad}) => {
-//   const total = good + neutral + bad
-//   const  posPercent = good/total
-//   return(
-//     <> 
-//     <p> positive {posPercent}%</p>
-//     </>
-//   )
-// }
-const Button = (props) => {
-  const {buttonAction, text} = props
-  return (
-  <button onClick={buttonAction}>{text}</button>
-  )
-}
-
 const Statistics = (props) => {
   const {good, neutral, bad } = props
   const all = good+neutral+bad
@@ -51,24 +25,55 @@ return (
 )
 }
 
+const ShowAvg = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  const  avg = ((1*good) + (-1*bad))/total
+  return(
+    <> 
+    <p>avg {avg}</p>
+    </>
+  )
+}
+
+const ShowPosPercent = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  const  posPercent = good/total
+  return(
+    <> 
+    <p> positive {posPercent}%</p>
+    </>
+  )
+}
+
+const Button = (props) => {
+  const {buttonAction, text} = props
+  return (
+  <button onClick={buttonAction}>{text}</button>
+  )
+}
 
 const StatisticsLine = ({text, values}) => {
   const [good, neutral, bad] = values
   const all = good+neutral+bad
   const weightedSum = (1*good) + (0*neutral) + (-1*bad)
   const avg = (weightedSum/all).toFixed(2)
-  const posPercent = good/all
+  const posPercent = (good/all).toFixed(2)
 
   const GetStats = ({label, val}) => {
     if (label!="positive"){
-      return <p>{label}: {val}</p>
-    }
-    else{
-      return <p>{label}: {val.toFixed(2)}%</p>
-    }
+      return (
+      <>
+        <td>{label}</td> <td>{val}</td>
+      </>
+      )
+      }
+      return (
+      <>
+        <td>{label}</td> <td>{val}%</td>
+      </>
+      )
 
   }
-
   switch(text){
     case "good":
       return <GetStats label={text} val={good}/>
@@ -110,14 +115,18 @@ const App = () => {
       <Button buttonAction={actionBad} text={'bad'}/>
 
       <h1>statistics</h1>
-      <StatisticsLine text={'good'} values={allVals}/>
-      <StatisticsLine text={'neutral'} values={allVals}/>
-      <StatisticsLine text={'bad'} values={allVals}/>
-      <StatisticsLine text={'all'} values={allVals}/>
-      <StatisticsLine text={'average'} values={allVals}/>
-      <StatisticsLine text={'positive'} values={allVals}/>
+      <table>
+          <tr> <StatisticsLine text={'good'} values={allVals}/> </tr>
+          <tr> <StatisticsLine text={'neutral'} values={allVals}/> </tr>
+          <tr> <StatisticsLine text={'bad'} values={allVals}/> </tr>
+          <tr> <StatisticsLine text={'all'} values={allVals}/> </tr>
+          <tr> <StatisticsLine text={'average'} values={allVals}/> </tr>
+          <tr> <StatisticsLine text={'positive'} values={allVals}/> </tr>
+      </table>
         
     </div>
   )
 }
 export default App
+
+
